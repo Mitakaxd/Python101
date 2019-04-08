cf225fc74007f56d065ea0535ffc5af4bca950d4
Mitakaxd <mitaka.g.g@abv.bg>
2019-04-08 18:33:25 +0300

    Context Managers added

---
+++ week07/context_managers.py
@@ -0,0 +1,34 @@
+from contextlib import contextmanager
+import datetime
+from time import sleep
+@contextmanager
+def performance(file_name):
+    with open(file_name,'a') as f:
+        time_now = datetime.datetime.now()
+        yield
+        time_elapsed = (datetime.datetime.now() - time_now)
+        f.write(str(datetime.datetime.now()) + " Execution time: " + str(time_elapsed) + '\n')
+
+# def myTestfunc():
+#     with performance('log.txt'):
+#         sleep(1)
+
+# myTestfunc()
+# myTestfunc()
+@contextmanager
+def assertRaises(exception,msg=None):
+    try:
+        yield
+    except exception:
+        pass
+    else:
+        raise Exception(msg)
+
+
+def testing_assert_raises():
+    with assertRaises(IndexError,"I didnt hit anything"):
+        lst = []
+        print(lst[3])
+
+
+testing_assert_raises()
