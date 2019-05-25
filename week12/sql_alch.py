d7097dfa1529c5e5b33c9a8e902660f1b4ad0b49
Mitakaxd <mitaka.g.g@abv.bg>
2019-05-25 17:56:08 +0300

    delete duplicates

--- week12/sql_alch.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.sql import select
-from sqlalchemy import Table,Column,Integer,String,Metadata
-metadata= Metadata()
-engine = create_engine('sqlite:///:my_users:', echo=True)
